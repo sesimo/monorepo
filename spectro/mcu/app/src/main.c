387580dc0e3a36b7d2a077ae34b7f9f795d6b10f
sigmundklaa <sigmundklaa@outlook.com>
2025-02-04 13:03:02 +0100

    spectro: Structure as module with app
    Outer scope is a module, while the application resides in the `app`
    directory.

--- spectro/mcu/app/src/main.c
+++ spectro/mcu/app/src/main.c
@@ -2,8 +2,14 @@
 #include <stdio.h>
 #include <zephyr/kernel.h>
 
+static const struct device *dev = DEVICE_DT_GET_ANY(toshiba_tcd1304);
+
 int main(void)
 {
+        if (!device_is_ready(dev)) {
+                (void)printf("CCD not set up\n");
+        }
+
         for (;;) {
                 (void)printf("Hello World\n");
 

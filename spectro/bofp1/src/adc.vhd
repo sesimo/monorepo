bd7226a6808bd259d329c066efabb697f6f1ec8a
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2025-02-10 22:43:13 +0100

    spectro: fpga: Initial

---
+++ spectro/bofp1/src/adc.vhd
@@ -0,0 +1,104 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity ads8329 is
+    generic (
+        G_RESOLUTION: integer := 16;
+        G_CLK_FREQ: integer := 100_000_000;
+        G_MOD_FREQ: integer := 10_000_000
+    );
+
+    port (
+        i_clk: in std_logic;
+        i_rst_n: in std_logic;
+        i_start: in std_logic;
+
+        o_rdy: out std_logic;
+        o_buf: out std_logic_vector(G_RESOLUTION-1 downto 0)
+    );
+end entity ads8329;
+
+architecture rtl of ads8329 is
+    type t_state is (S_IDLE, S_CONVERTING, S_READ);
+    signal r_state: t_state;
+
+    signal r_enable: std_logic;
+    constant c_clk_div: integer := G_CLK_FREQ / G_MOD_FREQ;
+
+    -- TODO: instantiate SPI
+    -- spi port (
+    --     i_clk => i_clk,
+    --     i_rst_n => i_rst_n,
+    --     i_enable => i_enable,
+    --     o_active => o_active,
+    --     o_buf => o_buf
+    -- )
+
+    -- Lines to ADC hardware
+    signal o_stconv: std_logic;
+    signal i_eoc: std_logic;
+
+    -- SPI inputs
+    signal o_spi_enable: std_logic;
+    signal i_spi_active: std_logic;
+begin
+    -- Generate enable signal
+    p_enable: process(i_clk, i_rst_n) 
+        variable v_count: integer := 0;
+    begin
+        if i_rst_n = '0' then
+            v_count := 0;
+            r_enable <= '0';
+        elsif rising_edge(i_clk) then
+            if v_count = c_clk_div then
+                v_count := 0;
+                r_enable <= '1';
+            else
+                v_count := v_count + 1;
+                r_enable <= '0';
+            end if;
+        end if;
+    end process p_enable;
+
+    -- State machine
+    p_conv: process(i_clk, i_rst_n)
+    begin
+        if i_rst_n = '0' then
+            r_state <= S_IDLE;
+
+            o_rdy <= '0';
+            o_buf <= (others => '0');
+            
+            o_stconv <= '0';
+            i_eoc <= '0';
+        elsif rising_edge(i_clk) and r_enable = '1' then
+            o_stconv <= '0';
+            o_rdy <= '0';
+            o_spi_enable <= '0';
+
+            case r_state is
+                when S_IDLE =>
+                    -- Wait for start signal
+                    if i_start = '1' then
+                        o_stconv <= '1';
+                        r_state <= S_CONVERTING;
+                    end if;
+                when S_CONVERTING =>
+                    -- Started, wait for conversion to complete
+                    if i_eoc = '1' then
+                        o_spi_enable <= '1';
+                        r_state <= S_READ;
+                    end if;
+                when S_READ =>
+                    -- Conversion done, read out
+                    if i_spi_active = '0' then
+                        o_rdy <= '1';
+                        r_state <= S_IDLE;
+                    end if;
+            end case;
+        end if;
+    end process p_conv;
+
+end architecture rtl;
---
+++ spectro/bofp1/tests/tb_adc.vhd
@@ -0,0 +1,60 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+
+entity tb_adc is
+    generic (
+        G_RESOLUTION: integer := 16;
+        G_CLK_FREQ: integer := 100_000_000
+    );
+end entity tb_adc;
+
+architecture bhv of tb_adc is
+    signal r_clk: std_logic := '0';
+    signal r_rst_n: std_logic := '0';
+    signal r_start: std_logic := '0';
+    signal r_rdy: std_logic := '0';
+    signal r_buf: std_logic_vector(G_RESOLUTION-1 downto 0);
+
+    constant c_clk_period: time := (1.0 / real(G_CLK_FREQ)) * (1 sec);
+begin
+    u_adc: entity work.ads8329(rtl) generic map(
+        G_CLK_FREQ => G_CLK_FREQ,
+        G_RESOLUTION => G_RESOLUTION
+    )
+    port map(
+        i_clk => r_clk,
+        i_rst_n => r_rst_n,
+        i_start => r_start,
+
+        o_rdy => r_rdy,
+        o_buf => r_buf
+    );
+
+    p_clk: process
+    begin
+        r_clk <= '0';
+        wait for c_clk_period / 2;
+        r_clk <= '1';
+        wait for c_clk_period / 2;
+    end process p_clk;
+
+    p_rst: process
+    begin
+        r_rst_n <= '0';
+        wait for c_clk_period * 3;
+        r_rst_n <= '1';
+        wait;
+    end process p_rst;
+
+    p_main: process
+    begin
+        wait for c_clk_period * 1000;
+        assert false report "test ended" severity error;
+
+        wait;
+    end process p_main;
+
+end architecture bhv;

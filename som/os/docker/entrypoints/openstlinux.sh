525b48d9fc5f817cc3adb2d39a83633ed599b2e3
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2025-02-10 19:40:16 +0100

    store (squash)

---
+++ som/os/docker/entrypoints/openstlinux.sh
@@ -0,0 +1,4 @@
+
+DISTRO=openstlinux-weston MACHINE=stm32mp1 source layers/meta-st/scripts/envsetup.sh
+
+exec "$@"
---
+++ som/os/docker/setup/openstlinux.sh
@@ -0,0 +1,7 @@
+
+
+repo init -u https://github.com/STMicroelectronics/oe-manifest.git \
+    -b scarthgap
+repo sync -j16
+
+

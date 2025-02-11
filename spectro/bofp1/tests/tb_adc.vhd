66eba63ed6654e8b2c2b094ce90519c22bde91f4
sigmundklaa <sigmundklaa@outlook.com>
2025-02-11 16:20:38 +0100

    squash

--- spectro/bofp1/tests/tb_adc.vhd
+++ spectro/bofp1/tests/tb_adc.vhd
@@ -18,6 +18,14 @@ architecture bhv of tb_adc is
     signal r_rdy: std_logic := '0';
     signal r_buf: std_logic_vector(G_RESOLUTION-1 downto 0);
 
+    signal r_stconv: std_logic := '0';
+    signal r_eoc: std_logic := '0';
+
+    constant c_ads8329_pins: work.p_ads8329.t_pins := (
+        i_stconv => r_stconv,
+        o_eoc => r_eoc
+    );
+
     constant c_clk_period: time := (1.0 / real(G_CLK_FREQ)) * (1 sec);
 begin
     u_adc: entity work.ads8329(rtl) generic map(
@@ -48,6 +56,12 @@ begin
         r_rst_n <= '1';
         wait;
     end process p_rst;
+
+    p_eoc: process
+    begin
+        wait for c_clk_period * 5;
+        wait;
+    end process p_eoc;
 
     p_main: process
     begin

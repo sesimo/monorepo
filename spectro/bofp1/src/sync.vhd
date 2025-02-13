ce65d78b81bc3ab323750d542f8869ca4631c89f
sigmundklaa <sigmundklaa@outlook.com>
2025-02-13 16:59:27 +0100

    spectro: bofp1: Add counter entity

--- spectro/bofp1/src/sync.vhd
+++ spectro/bofp1/src/sync.vhd
@@ -36,6 +36,54 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+entity counter is
+    generic (
+        G_WIDTH: integer
+    );
+
+    port (
+        i_clk: in std_logic;
+        i_start: in std_logic;
+        i_cyc_cnt: in std_logic_vector(G_WIDTH-1 downto 0);
+        i_rst_n: in std_logic;
+        o_int: out std_logic
+    );
+end entity counter;
+
+architecture rtl of counter is
+    signal r_count: integer;
+begin
+
+    p_count: process(i_clk)
+    begin
+        if rising_edge(i_clk) then
+            if i_rst_n = '0' then
+                r_count <= 0;
+                o_int <= '0';
+            else
+                r_count <= r_count + 1;
+                o_int <= '0';
+
+                if r_count = 0 then
+                    o_int <= '1';
+
+                    -- Only restart when i_start is high
+                    if i_start = '0' then
+                        r_count <= 0;
+                    end if;
+                elsif r_count >= unsigned(i_cyc_cnt) - 1 then
+                    r_count <= 0;
+                end if;
+            end if;
+        end if;
+    end process p_count;
+
+end architecture rtl;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
 -- brief Enable generator
 entity enable is
     generic (
@@ -50,24 +98,15 @@ entity enable is
 end entity enable;
 
 architecture rtl of enable is
-    signal r_count: integer;
 begin
-
-    -- Generate enable signal
-    p_enable: process(i_clk) 
-    begin
-        if rising_edge(i_clk) then
-            o_enable <= '0';
-            r_count <= r_count + 1;
-
-            if i_rst_n = '0' then
-                r_count <= 0;
-            elsif r_count = 0 then
-                o_enable <= '1';
-            elsif r_count >= unsigned(i_clk_div) - 1 then
-                r_count <= 0;
-            end if;
-        end if;
-    end process p_enable;
-
+    u_counter: entity work.counter(rtl) generic map (
+        G_WIDTH => G_WIDTH
+    )
+    port map(
+        i_clk => i_clk,
+        i_start => '1',
+        i_cyc_cnt => i_clk_div,
+        i_rst_n => i_rst_n,
+        o_int => o_enable
+    );
 end architecture rtl;

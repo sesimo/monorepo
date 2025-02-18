c9b2d16e83b6729a75c62a0e2818ab181da79383
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2025-02-18 11:31:01 +0100

    spectro: bofp1: Simplify ADC

--- spectro/bofp1/src/adc.vhd
+++ spectro/bofp1/src/adc.vhd
@@ -13,18 +13,16 @@ entity ads8329 is
         i_clk: in std_logic;
         i_rst_n: in std_logic;
         i_start: in std_logic;
-        o_busy: out std_logic;
 
         i_pin_eoc: in std_logic;
         o_pin_stconv: out std_logic;
 
-        i_spi_rdy: in std_logic;
         o_spi_enable: out std_logic
     );
 end entity ads8329;
 
 architecture rtl of ads8329 is
-    type t_state is (S_IDLE, S_CONVERTING, S_READ);
+    type t_state is (S_IDLE, S_CONVERTING);
     signal r_state: t_state;
 
     signal r_enable: std_logic;
@@ -53,8 +51,6 @@ begin
         if rising_edge(i_clk) then
             if i_rst_n = '0' then
                 r_state <= S_IDLE;
-
-                o_busy <= '0';
                 
                 o_pin_stconv <= '0';
                 o_spi_enable <= '0';
@@ -66,7 +62,6 @@ begin
                         -- Wait for start signal
                         if i_start = '1' then
                             o_pin_stconv <= '1';
-                            o_busy <= '1';
 
                             r_state <= S_CONVERTING;
                         end if;
@@ -74,14 +69,6 @@ begin
                         -- Started, wait for conversion to complete
                         if r_eoc = '1' then
                             o_spi_enable <= '1';
-
-                            r_state <= S_READ;
-                        end if;
-                    when S_READ =>
-                        -- Conversion done, read out
-                        if i_spi_rdy = '1' then
-                            o_busy <= '0';
-                            o_spi_enable <= '0';
 
                             r_state <= S_IDLE;
                         end if;

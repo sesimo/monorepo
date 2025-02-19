8db1e0d194d11bcbb9ea9281befd614c6457be40
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2025-02-19 16:50:24 +0100

    spectro: bofp1: tests: Improve PWM tb

--- spectro/bofp1/tests/tb_pwm.vhd
+++ spectro/bofp1/tests/tb_pwm.vhd
@@ -8,7 +8,10 @@ use std.env.stop;
 entity tb_pwm is
     generic (
         G_CLK_FREQ: integer := 100_000_000;
-        G_WIDTH: integer := 12
+        G_WIDTH: integer := 12;
+
+        G_PERIOD_CYC: integer := 250;
+        G_PULSE_CYC: integer := 20
     );
 end entity tb_pwm;
 
@@ -16,12 +19,15 @@ architecture bhv of tb_pwm is
     signal r_clk: std_logic := '0';
     signal r_rst_n: std_logic := '0';
     signal r_period: std_logic_vector(G_WIDTH-1 downto 0) := std_logic_vector(
-        to_unsigned(250, G_WIDTH));
+        to_unsigned(G_PERIOD_CYC, G_WIDTH));
     signal r_pulse: std_logic_vector(G_WIDTH-1 downto 0) := std_logic_vector(
-        to_unsigned(20, G_WIDTH));
+        to_unsigned(G_PULSE_CYC, G_WIDTH));
     signal r_oclk: std_logic;
 
     constant c_clk_period: time := (1.0 / real(G_CLK_FREQ)) * (1 sec);
+
+    constant c_high_time: time := c_clk_period * G_PULSE_CYC;
+    constant c_low_time: time := c_clk_period * (G_PERIOD_CYC - G_PULSE_CYC);
 begin
     u_pwm: entity work.pwm(rtl) generic map (
         G_WIDTH => G_WIDTH
@@ -40,8 +46,24 @@ begin
     p_main: process
     begin
         wait until r_rst_n = '1';
-        wait for 10000 ns;
-        assert false report "test" severity note;
+        wait until rising_edge(r_oclk) for c_clk_period * 2;
+        assert r_oclk = '1'
+        report "Oclk should have been started" severity failure;
+
+        report "High time: " & time'image(c_high_time) severity note;
+        report "Low time: " & time'image(c_low_time) severity note;
+        
+        for i in 0 to 10 loop
+            wait until falling_edge(r_oclk);
+            assert falling_edge(r_oclk) and r_oclk'delayed(c_high_time) = '1'
+            report "Oclk should be high" severity failure;
+
+            wait until rising_edge(r_oclk);
+            assert r_oclk'delayed(c_low_time) = '0'
+            report "Oclk should be low" severity failure;
+        end loop;
+
+        wait for c_clk_period * 10;
 
         stop;
     end process p_main;

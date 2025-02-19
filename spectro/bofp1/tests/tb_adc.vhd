8b552a35361a6982e6c85498dfa027a16d884ce1
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2025-02-19 12:57:21 +0100

    spectro: bofp1: tests: Fix adc tb

--- spectro/bofp1/tests/tb_adc.vhd
+++ spectro/bofp1/tests/tb_adc.vhd
@@ -24,12 +24,12 @@ architecture bhv of tb_adc is
 
     signal r_spi_enable: std_logic := '0';
     signal r_spi_rdy: std_logic := '0';
+    signal r_rd_en: std_logic;
 
     constant c_clk_period: time := (1.0 / real(G_CLK_FREQ)) * (1 sec);
     constant c_mod_period: time := c_clk_period * G_CLK_DIV;
 begin
     u_adc: entity work.ads8329(rtl) generic map(
-        G_RESOLUTION => G_RESOLUTION,
         G_CLK_DIV => G_CLK_DIV
     )
     port map(
@@ -40,10 +40,7 @@ begin
         i_pin_eoc => r_eoc,
         o_pin_stconv => r_stconv,
 
-        o_spi_enable => r_spi_enable,
-        i_spi_rdy => r_spi_rdy,
-
-        o_busy => r_busy
+        o_rd_en => r_rd_en
     );
 
     p_clk: process
@@ -62,37 +59,57 @@ begin
         wait;
     end process p_rst;
 
+    p_convert: process
+    begin
+        if r_rst_n = '0' then
+            wait until r_rst_n <= '1';
+        end if;
+
+        wait until r_stconv = '1';
+        r_eoc <= '0';
+
+        -- Total wait time: roughly 18 CCLKs at nominal freqency
+        wait for 807 ns; 
+        r_eoc <= '1';
+    end process;
+
     p_main: process
     begin
         wait until r_rst_n = '1';
-        wait for c_mod_period;
 
-        -- Start conversion
-        assert r_busy = '0' report "Busy should not be set" severity failure;
-        assert r_stconv = '0' report "STconv should not be set" severity failure;
-        r_start <= '1';
+        for i in 0 to 1 loop
+            assert r_stconv = '0'
+            report "STconv should be low" severity failure;
+            r_start <= '1';
 
-        -- Wait for conversion to have started, signal end of conversion
-        wait for c_mod_period;
-        assert r_busy = '1' report "Busy should be set" severity failure;
-        assert r_stconv = '1' report "STconv should be set" severity failure;
-        assert r_spi_enable = '0' report "SPI enable should not be set"
-            severity failure;
-        r_eoc <= '1';
-        r_start <= '0';
+            wait until r_stconv = '1' for c_mod_period*1.5;
+            assert r_stconv = '1'
+            report "STconv should be high" severity failure;
+            r_start <= '0';
 
-        -- Start SPI read, signal read done
-        wait for c_mod_period;
-        assert r_spi_enable = '1' report "SPI enable should be set"
-            severity failure;
-        r_spi_rdy <= '1';
-        
-        -- Wait for read to complete
-        wait for c_mod_period;
-        assert r_busy = '0' report "Busy should be cleared" severity failure;
+            wait until r_stconv = '0' for 120 ns;
+            assert r_stconv = '0'
+            report "STconv should have been cleared" severity failure;
 
-        wait for c_mod_period * 2;
-        assert r_busy = '0' report "Should remain busy" severity failure;
+            assert r_rd_en = '0'
+            report "RD en should be low" severity failure;
+            wait until (r_rd_en = '1' or r_eoc = '1') for 900 ns;
+            assert r_eoc = '1'
+            report "EOC should have been set high" severity failure;
+            assert r_rd_en = '0'
+            report "RD en should be kept low" severity failure;
+
+            wait until r_rd_en = '1' for c_mod_period;
+            assert r_rd_en = '1'
+            report "RD en should be set high" severity failure;
+
+            wait until r_rd_en = '0' for c_clk_period * 1.05;
+            assert r_rd_en = '0'
+            report "RD en should have been cleared" severity failure;
+
+            wait for c_mod_period;
+        end loop;
+
         stop;
     end process p_main;
 

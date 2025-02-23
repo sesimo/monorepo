b11d5ffaf18e67b4def0cd7d588352a51f35927c
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2025-02-23 13:35:44 +0100

    spectro: bofp1: spi: main: Use external clock
    SCLK should not be generated by logic, but instead vendor specific
    clock. This can be assumed to have the same phase as the main clock
    so CDC concerns are ignored for now

--- spectro/bofp1/src/spi/spi_main.vhd
+++ spectro/bofp1/src/spi/spi_main.vhd
@@ -6,9 +6,7 @@ use ieee.numeric_std.all;
 entity spi_main is
     generic (
         G_MODE: integer range 0 to 3 := 1;
-        G_DATA_WIDTH: integer := 8;
-
-        G_CLK_DIV: integer := 2
+        G_DATA_WIDTH: integer := 8
     );
     port (
         i_clk: in std_logic;
@@ -17,8 +15,8 @@ entity spi_main is
         i_data: in std_logic_vector(G_DATA_WIDTH-1 downto 0);
 
         i_miso: in std_logic;
+        i_sclk: in std_logic;
         o_mosi: out std_logic;
-        o_sclk: out std_logic;
         o_cs_n: out std_logic;
         
         o_data: out std_logic_vector(G_DATA_WIDTH-1 downto 0);
@@ -27,45 +25,16 @@ entity spi_main is
 end entity spi_main;
 
 architecture rtl of spi_main is
-    signal r_sclk_buf: std_logic;
-    signal r_sclk_rst_n: std_logic;
     signal r_cs_n_buf: std_logic;
 
     signal r_start_ext: std_logic;
+    signal r_sample_done: std_logic;
+    signal r_shift_done: std_logic;
 
     type t_state is (S_IDLE, S_STARTING, S_RUNNING, S_STOPPING);
     signal r_state: t_state;
-
-    constant c_period: unsigned := to_unsigned(G_CLK_DIV, 8);
-    constant c_pulse: unsigned := c_period / 2;
-
-    -- Convert a boolean to std_logic '1' or '0'
-    function f_bool_logic(b: in boolean) return std_logic is
-    begin
-        if b then
-            return '1';
-        end if;
-
-        return '0';
-    end function f_bool_logic;
 begin
-    o_sclk <= r_sclk_buf;
-    r_sclk_rst_n <= f_bool_logic(not(i_rst_n = '0' or (r_state = S_IDLE)));
-    
     o_cs_n <= r_cs_n_buf;
-
-    -- Drive SCLK
-    u_sclk: entity work.pwm(rtl)
-        generic map (
-            G_WIDTH => 8
-        )
-        port map (
-            i_clk => i_clk,
-            i_rst_n => r_sclk_rst_n,
-            i_period => std_logic_vector(c_period),
-            i_pulse => std_logic_vector(c_pulse),
-            o_clk => r_sclk_buf
-        );
 
     -- Common SPI entity, sampling in and shifting out
     u_spi_common: entity work.spi_common
@@ -74,13 +43,16 @@ begin
             G_DATA_WIDTH => G_DATA_WIDTH
         )
         port map(
-            i_sclk => r_sclk_buf,
+            i_sclk => i_sclk,
             i_cs_n  => r_cs_n_buf,
             i_in => i_miso,
             o_out => o_mosi,
 
             i_data => i_data,
-            o_data => o_data
+            o_data => o_data,
+
+            o_sample_done => r_sample_done,
+            o_shift_done => r_shift_done
         );
 
     -- Detect the start condition and prolong it for one SCLK cycle
@@ -94,19 +66,17 @@ begin
                 if i_start = '1' then
                     r_start_ext <= '1';
                     v_sclk_last := '1';
-                elsif r_sclk_buf /= v_sclk_last and r_sclk_buf = '1' then
+                elsif i_sclk /= v_sclk_last and i_sclk = '1' then
                     -- Clear at next high edge of SCLK
                     r_start_ext <= '0';
                 end if;
 
-                v_sclk_last := r_sclk_buf;
+                v_sclk_last := i_sclk;
             end if;
         end if;
     end process p_detect_start;
 
     p_handle_state: process(i_clk)
-        variable v_cyc_cnt: integer;
-        variable v_last: std_logic;
     begin
         if rising_edge(i_clk) then
             if i_rst_n = '0' then
@@ -124,22 +94,15 @@ begin
                         end if;
                     when S_STARTING =>
                         r_state <= S_RUNNING;
-                        v_cyc_cnt := 0;
                     when S_RUNNING =>
-                        if r_sclk_buf = '0' and r_sclk_buf /= v_last then
-                            v_cyc_cnt := v_cyc_cnt + 1;
-
-                            if v_cyc_cnt >= G_DATA_WIDTH then
-                                o_rdy <= '1';
-                                r_state <= S_STOPPING;
-                            end if;
+                        if r_sample_done = '1' and r_shift_done = '1' then
+                            r_state <= S_STOPPING;
+                            r_cs_n_buf <= '1';
+                            o_rdy <= '1';
                         end if;
                     when S_STOPPING =>
-                        r_cs_n_buf <= '1';
                         r_state <= S_IDLE;
                 end case;
-
-                v_last := r_sclk_buf;
             end if;
         end if;
     end process p_handle_state;

f415bb19906bce526063643c8257d7f8c0e3916c
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2025-02-26 08:10:12 +0100

    spectro: mcu: Update app

--- spectro/mcu/app/src/main.c
+++ spectro/mcu/app/src/main.c
@@ -1,19 +1,9 @@
 
-#include <stdio.h>
 #include <zephyr/kernel.h>
-
-static const struct device *dev = DEVICE_DT_GET_ANY(toshiba_tcd1304);
 
 int main(void)
 {
-        if (!device_is_ready(dev)) {
-                (void)printf("CCD not set up\n");
-        }
-
         for (;;) {
-                (void)printf("Hello World\n");
-
-                k_sleep(K_MSEC(1000));
         }
 
         return 0;

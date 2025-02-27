045b5a0bcaf53eb274d81b7e3c8cd1dae6c44a69
sigmundklaa <sigmundklaa@outlook.com>
2025-02-27 13:31:00 +0100

    spectro: mcu: Update app

--- spectro/mcu/app/src/main.c
+++ spectro/mcu/app/src/main.c
@@ -1,10 +1,25 @@
 
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(app);
 
 int main(void)
 {
-        for (;;) {
-        }
+#if DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_shell_uart), zephyr_cdc_acm_uart)
+	const struct device *dev;
+	uint32_t dtr = 0;
 
-        return 0;
-}
+	dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_shell_uart));
+	if (!device_is_ready(dev) || usb_enable(NULL)) {
+		return 0;
+	}
+
+	while (!dtr) {
+		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+		k_sleep(K_MSEC(100));
+	}
+#endif
+    LOG_INF("starting");
+	return 0;
+};

144c0e66bec95975e89d2993661fe1e795874b13
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2025-03-01 20:33:57 +0100

    spectro: bofp1: adc: Use pulse for STconv pulse

--- spectro/bofp1/src/adc.vhd
+++ spectro/bofp1/src/adc.vhd
@@ -24,13 +24,12 @@ architecture rtl of ads8329 is
     type t_state is (S_IDLE, S_START, S_CONVERTING);
     signal r_state: t_state;
 
-    signal r_stconv: std_logic;
-    signal r_stconv_fall: std_logic;
+    signal r_stconv_rise: std_logic;
 
     signal r_eoc: std_logic;
     signal r_cdc_eoc: std_logic;
 begin
-    u_counter: entity work.counter(rtl)
+    u_stconv_pulse: entity work.pulse(rtl)
         generic map(
             G_WIDTH => 8
         )
@@ -38,8 +37,8 @@ begin
             i_clk => i_clk,
             i_rst_n => i_rst_n,
             i_cyc_cnt => std_logic_vector(to_unsigned(G_STCONV_HOLD_CYC, 8)),
-            i_start => r_stconv,
-            o_int => r_stconv_fall
+            i_en => r_stconv_rise,
+            o_out => o_pin_stconv
         );
 
     p_eoc: process(i_clk)
@@ -63,38 +62,23 @@ begin
         end if;
     end process p_eoc;
 
-    p_stconv: process(i_clk)
-    begin
-        if rising_edge(i_clk) then
-            if i_rst_n = '0' then
-                o_pin_stconv <= '0';
-            else
-                if r_stconv = '1' then
-                    o_pin_stconv <= '1';
-                elsif r_stconv_fall = '1' then
-                    o_pin_stconv <= '0';
-                end if;
-            end if;
-        end if;
-    end process p_stconv;
-
     p_conv: process(i_clk)
     begin
         if rising_edge(i_clk) then
             if i_rst_n = '0' then
                 r_state <= S_IDLE;
                 
-                r_stconv <= '0';
+                r_stconv_rise <= '0';
                 o_rd_en <= '0';
             else 
                 o_rd_en <= '0';
-                r_stconv <= '0';
+                r_stconv_rise <= '0';
 
                 case r_state is
                     when S_IDLE =>
                         -- Wait for start signal
                         if i_start = '1' then
-                            r_stconv <= '1';
+                            r_stconv_rise <= '1';
 
                             r_state <= S_START;
                         end if;

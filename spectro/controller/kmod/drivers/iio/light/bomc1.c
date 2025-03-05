6a4f8ea4ad4f8e1fef158ca1f0887c5cc1a2205b
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2025-03-05 22:35:33 +0100

    bohr controller intial

---
+++ spectro/controller/kmod/drivers/iio/light/bomc1.c
@@ -0,0 +1,20 @@
+
+#include <linux/module.h>
+#include <linux/init.h>
+
+static int __init bomc1_init(void)
+{
+        pr_info("BOCM1 loaded");
+        return 0;
+}
+
+static void __exit bomc1_exit(void)
+{
+}
+
+module_init(bomc1_init);
+module_exit(bomc1_exit);
+
+MODULE_AUTHOR("Sigmund Klåpbakken");
+MODULE_LICENSE("Proprietary");
+MODULE_DESCRIPTION("SESIMO BOMC1 driver module");

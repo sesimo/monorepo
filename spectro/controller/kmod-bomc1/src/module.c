8525a7f741d417d10988ce8b4e3038a4980f299c
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2025-03-13 22:42:29 +0100

    spectro: controller: Module/usb initial

--- spectro/controller/kmod-bomc1/src/module.c
+++ spectro/controller/kmod-bomc1/src/module.c
@@ -1,21 +1,37 @@
 
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/usb.h>
 
 #include "module.h"
 
-static int __init bomc1_init(void)
+static const struct usb_device_id bomc1_id_table[] = {
+        {USB_DEVICE(0xf005, 0x001)},
+        {/* sentinel */},
+};
+
+MODULE_DEVICE_TABLE(usb, bomc1_id_table);
+
+static int bomc1_probe(struct usb_interface *intf,
+                       const struct usb_device_id *id)
 {
-        pr_info("BOCM1 loaded");
+        pr_info("Probed");
         return 0;
 }
 
-static void __exit bomc1_exit(void)
+static void bomc1_disconnect(struct usb_interface *intf)
 {
+        pr_info("Disconnect");
 }
 
-module_init(bomc1_init);
-module_exit(bomc1_exit);
+static struct usb_driver bomc1_driver = {
+        .name = "bomc1",
+        .probe = bomc1_probe,
+        .disconnect = bomc1_disconnect,
+        .id_table = bomc1_id_table,
+};
+
+module_usb_driver(bomc1_driver);
 
 MODULE_AUTHOR(SSM_MOD_AUTHOR);
 MODULE_LICENSE(SSM_MOD_LICENSE);
--- spectro/controller/kmod-bomc1/src/module.h
+++ spectro/controller/kmod-bomc1/src/module.h
@@ -2,7 +2,7 @@
 #ifndef MODULE_H__
 #define MODULE_H__
 
-#define SSM_MOD_LICENSE "Proprietary"
+#define SSM_MOD_LICENSE "GPL"
 #define SSM_MOD_AUTHOR "Sigmund Klåpbakken"
 
 #endif /* MODULE_H__ */

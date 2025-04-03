9a8e1c020b7eeaf21898168014e50e3cd53fe2e7
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2025-04-03 16:54:45 +0200

    spectro: bofp1: spi: Simplify commoon func

--- spectro/bofp1/src/spi/spi_common.vhd
+++ spectro/bofp1/src/spi/spi_common.vhd
@@ -22,7 +22,7 @@ entity spi_common is
         o_out: out std_logic;
 
         i_data: in std_logic_vector(G_DATA_WIDTH-1 downto 0);
-        o_data_shf: out std_logic_vector(G_DATA_WIDTH-1 downto 0);
+        o_data: out std_logic_vector(G_DATA_WIDTH-1 downto 0);
 
         o_sample_done: inout std_logic;
         o_shift_done: inout std_logic
@@ -30,59 +30,92 @@ entity spi_common is
 end entity;
 
 architecture rtl of spi_common is
-    signal r_sample_shf: std_logic_vector(G_DATA_WIDTH-1 downto 0);
+    signal r_rst_n_mux: std_logic;
 
-    signal r_rst_mux: boolean;
+    -- TODO:
+    constant c_cnt_bits: natural := 5; --bits_needed(G_DATA_WIDTH);
 begin
-    o_data_shf <= r_sample_shf;
+    r_rst_n_mux <= '0' when (i_cs_n /= '0' or i_rst_n = '0') else '1';
 
-    r_rst_mux <= i_cs_n /= '0' or i_rst_n = '0';
-
-    -- Shift data out on `o_out`
-    p_shift: process(i_clk)
-        variable v_buf: std_logic_vector(G_DATA_WIDTH-1 downto 0);
-        variable v_count: integer range 0 to int_max(G_DATA_WIDTH);
+    b_shf: block
+        signal r_shf_cnt: std_logic_vector(c_cnt_bits-1 downto 0);
+        signal r_next: std_logic;
     begin
-        if rising_edge(i_clk) then
-            o_shift_done <= '0';
 
-            if r_rst_mux then
-                v_count := 0;
-            elsif i_shift_en = '1' then
-                if v_count = 0 then
-                    v_buf := i_data;
-                end if;
+        -- Count the number of shifts done, rolling over to zero when reaching
+        -- G_DATA_WIDTH
+        u_shf_cnt: entity work.counter
+            generic map(
+                G_WIDTH => c_cnt_bits
+            )
+            port map(
+                i_clk => i_clk,
+                i_rst_n => r_rst_n_mux,
+                i_en => i_shift_en,
+                i_max => std_logic_vector(
+                    to_unsigned(G_DATA_WIDTH, c_cnt_bits)),
+                o_roll => o_shift_done,
+                o_cnt => r_shf_cnt
+            );
 
-                o_out <= v_buf(v_buf'high - v_count);
-                v_count := v_count + 1;
-
-                if v_count >= G_DATA_WIDTH then
-                    v_count := 0;
-                    o_shift_done <= '1';
+        -- Load next bit into the register to ensure that it is ready when
+        -- i_shift_en goes high
+        p_shf_load: process(i_clk)
+        begin
+            if rising_edge(i_clk) then
+                if r_rst_n_mux /= '0' then
+                    r_next <= i_data(i_data'high - to_integer(unsigned(r_shf_cnt)));
                 end if;
             end if;
-        end if;
-    end process p_shift;
+        end process p_shf_load;
 
-    -- Sample data and place it in the buffer. This notifies in `o_rdy`
-    p_sample: process(i_clk)
-        variable v_count: integer range 0 to int_max(G_DATA_WIDTH);
-    begin
-        if rising_edge(i_clk) then
-            o_sample_done <= '0';
-
-            if r_rst_mux then
-                v_count := 0;
-            elsif i_sample_en = '1' then
-                r_sample_shf(r_sample_shf'high - v_count) <= i_in;
-                v_count := v_count + 1;
-
-                if v_count >= G_DATA_WIDTH then
-                    v_count := 0;
-                    o_sample_done <= '1';
+        -- Shift data out on data line
+        p_shf_do: process(i_clk)
+        begin
+            if rising_edge(i_clk) then
+                if i_shift_en = '1' then
+                    o_out <= r_next;
                 end if;
             end if;
-        end if;
-    end process p_sample;
+        end process p_shf_do;
+
+    end block b_shf;
+
+    b_smpl: block
+        signal r_smpl_cnt: std_logic_vector(c_cnt_bits-1 downto 0);
+        signal r_smpl_buf: std_logic_vector(G_DATA_WIDTH-1 downto 0);
+    begin
+
+        -- Count the number of shifts done, rolling over to zero when reaching
+        -- G_DATA_WIDTH
+        u_smpl_cnt: entity work.counter
+            generic map(
+                G_WIDTH => c_cnt_bits
+            )
+            port map(
+                i_clk => i_clk,
+                i_rst_n => r_rst_n_mux,
+                i_en => i_sample_en,
+                i_max => std_logic_vector(
+                    to_unsigned(G_DATA_WIDTH, c_cnt_bits)),
+                o_roll => o_sample_done,
+                o_cnt => r_smpl_cnt
+            );
+
+        -- Load i_in into the buffer
+        p_smpl_do: process(i_clk)
+            variable v_index: integer;
+        begin
+            if rising_edge(i_clk) then
+                if i_sample_en = '1' then
+                    v_index := r_smpl_buf'high - to_integer(unsigned(r_smpl_cnt));
+                    r_smpl_buf(v_index) <= i_in;
+                end if;
+            end if;
+        end process p_smpl_do;
+
+        o_data <= r_smpl_buf;
+
+    end block b_smpl;
 
 end architecture rtl;

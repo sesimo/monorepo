6f9f360bc097622024b4fd2dd8872a866bc4a26f
sigmundklaa <sigmundklaa@outlook.com>
2025-04-08 15:35:12 +0200

    spectro: mcu: drivers: bofp1: Remove MCLK setting
    No longer supported

--- spectro/mcu/drivers/sensor/sesimo/bofp1/bofp1.c
+++ spectro/mcu/drivers/sensor/sesimo/bofp1/bofp1.c
@@ -65,19 +65,9 @@ int bofp1_read_reg(const struct device *dev, uint8_t addr, uint8_t *value)
 
 static uint32_t bofp1_mclk_freq(const struct device *dev)
 {
-        struct bofp1_data *data = dev->data;
         const struct bofp1_cfg *cfg = dev->config;
 
-        return cfg->clock_frequency /
-               (BOFP1_CLKDIV_MIN + BOFP1_CLKDIV_DERIV * data->clkdiv);
-}
-
-static uint32_t bofp1_mclk_div(const struct device *dev, uint32_t freq)
-{
-        const struct bofp1_cfg *cfg = dev->config;
-
-        return cfg->clock_frequency / (BOFP1_CLKDIV_DERIV * freq) -
-               BOFP1_CLKDIV_MIN / BOFP1_CLKDIV_DERIV;
+        return cfg->clock_frequency / cfg->clkdiv;
 }
 
 static uint32_t bofp1_psc_freq(const struct device *dev)
@@ -87,34 +77,11 @@ static uint32_t bofp1_psc_freq(const struct device *dev)
         return bofp1_mclk_freq(dev) / (cfg->psc + 1);
 }
 
-static uint8_t bofp1_clkdiv_packed(uint8_t clkdiv, uint8_t psc)
+static int bofp1_set_psc(const struct device *dev)
 {
-        return (psc << BOFP1_PSC_INDEX) | (clkdiv << BOFP1_CLKDIV_INDEX);
-}
-
-static int bofp1_set_sample_div(const struct device *dev, uint8_t div)
-{
-        uint8_t packed;
-        int status;
         const struct bofp1_cfg *cfg = dev->config;
-        struct bofp1_data *data = dev->data;
 
-        packed = bofp1_clkdiv_packed(div, cfg->psc);
-        status = bofp1_write_reg(dev, BOFP1_REG_CLKDIV, packed);
-        if (status != 0) {
-                return status;
-        }
-
-        data->clkdiv = div;
-
-        return 0;
-}
-
-static int bofp1_set_sample_freq(const struct device *dev, uint32_t freq)
-{
-        uint32_t mclk_freq = freq * BOFP1_DATA_CLKDIV;
-
-        return bofp1_set_sample_div(dev, bofp1_mclk_div(dev, mclk_freq));
+        return bofp1_write_reg(dev, BOFP1_REG_PSCDIV, cfg->psc);
 }
 
 static uint8_t bofp1_sh_div(const struct device *dev, uint32_t freq)
@@ -134,24 +101,32 @@ static int bofp1_set_integration_time(const struct device *dev,
         uint8_t div;
         int status;
         struct bofp1_data *data = dev->data;
+        k_spinlock_key_t key;
+
+        key = k_spin_lock(&data->lock);
 
         freq = 1000000000UL / time_ns;
         if (freq > bofp1_psc_freq(dev)) {
                 LOG_ERR("Integration time frequency must be lower than "
                         "prescaled frequency. Consider adjusting the "
                         "prescaler");
-                return -EINVAL;
+                status = -EINVAL;
+                goto exit;
         }
 
         div = bofp1_sh_div(dev, freq);
 
         status = bofp1_write_reg(dev, BOFP1_REG_CCD_SH, div);
         if (status != 0) {
-                return status;
+                goto exit;
         }
 
         data->shdiv = div;
-        return 0;
+
+exit:
+        k_spin_unlock(&data->lock, key);
+
+        return status;
 }
 
 static int bofp1_reset(const struct device *dev)
@@ -169,9 +144,6 @@ static int bofp1_attr_get(const struct device *dev, enum sensor_channel chan,
         }
 
         switch (attr) {
-        case SENSOR_ATTR_SAMPLING_FREQUENCY:
-                val->val1 = (int32_t)bofp1_mclk_freq(dev) / BOFP1_DATA_CLKDIV;
-                break;
         case SENSOR_ATTR_BOFP1_INTEGRATION:
                 val->val1 = bofp1_integration_time(dev, data->shdiv);
                 break;
@@ -191,8 +163,6 @@ static int bofp1_attr_set(const struct device *dev, enum sensor_channel chan,
         }
 
         switch (attr) {
-        case SENSOR_ATTR_SAMPLING_FREQUENCY:
-                return bofp1_set_sample_freq(dev, (uint32_t)val->val1);
         case SENSOR_ATTR_BOFP1_INTEGRATION:
                 return bofp1_set_integration_time(dev, (uint32_t)val->val1);
         default:
@@ -337,9 +307,9 @@ static int bofp1_init(const struct device *dev)
                 return status;
         }
 
-        status = bofp1_set_sample_div(dev, cfg->clkdiv_dt);
+        status = bofp1_set_psc(dev);
         if (status != 0) {
-                LOG_ERR("unable to set sampling div: %i", status);
+                LOG_ERR("unable to set prescaler: %i", status);
                 return status;
         }
 
@@ -363,7 +333,7 @@ static int bofp1_init(const struct device *dev)
                 .bus = SPI_DT_SPEC_INST_GET(inst_, BOFP1_SPI_OP,               \
                                             BOFP1_SPI_DELAY),                  \
                 .psc = DT_INST_PROP(inst_, prescaler),                         \
-                .clkdiv_dt = DT_INST_PROP(inst_, clkdiv),                      \
+                .clkdiv = DT_INST_PROP(inst_, clkdiv),                         \
                 .integration_time_dt = DT_INST_PROP(inst_, integration_time),  \
                 .busy_gpios = GPIO_DT_SPEC_INST_GET(inst_, busy_gpios),        \
                 .fifo_w_gpios =                                                \
--- spectro/mcu/drivers/sensor/sesimo/bofp1/bofp1.h
+++ spectro/mcu/drivers/sensor/sesimo/bofp1/bofp1.h
@@ -15,19 +15,8 @@
 #define BOFP1_REG_STREAM (0x0) /* Stream for entirety of transmission */
 #define BOFP1_REG_SAMPLE (0x1) /* Begin sample. Write only */
 #define BOFP1_REG_RESET  (0x2) /* Reset */
-#define BOFP1_REG_CLKDIV (0x3) /* Prescaler */
+#define BOFP1_REG_PSCDIV (0x3) /* Prescaler */
 #define BOFP1_REG_CCD_SH (0x4) /* Shutter frequncy (clock div)  */
-
-#define BOFP1_CLKDIV_MIN   (25)
-#define BOFP1_CLKDIV_MAX   (125)
-#define BOFP1_CLKDIV_DERIV ((BOFP1_CLKDIV_MAX - BOFP1_CLKDIV_MIN) / 7)
-
-#define BOFP1_DATA_CLKDIV (4) /* Data div in relation to master clock */
-
-/* In the clockdiv register, which is packed consisting of the two below
- * values */
-#define BOFP1_PSC_INDEX    (3)
-#define BOFP1_CLKDIV_INDEX (0)
 
 #define BOFP1_NUM_ELEMENTS_REAL        (3648)
 #define BOFP1_NUM_ELEMENTS_DUMMY_LEFT  (32)
@@ -43,7 +32,7 @@
 
 struct bofp1_cfg {
         uint8_t psc;
-        uint8_t clkdiv_dt;
+        uint8_t clkdiv;
         uint32_t integration_time_dt;
         uint32_t clock_frequency;
 
@@ -53,7 +42,6 @@ struct bofp1_cfg {
 };
 
 struct bofp1_data {
-        uint8_t clkdiv;
         uint8_t shdiv;
 
         struct gpio_callback busy_fall_cb;

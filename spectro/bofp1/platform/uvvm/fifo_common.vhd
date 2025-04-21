26e304f6f3c58273edf6a7587ce1d17da9185d4b
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2025-04-21 18:08:08 +0200

    spectro: bofp1: uvvm: Flesh out FIFO wrapper

--- spectro/bofp1/platform/uvvm/fifo_common.vhd
+++ spectro/bofp1/platform/uvvm/fifo_common.vhd
@@ -33,8 +33,21 @@ end entity fifo_common;
 
 architecture bhv of fifo_common is
     signal r_fifo: integer;
+
+    signal r_count: natural range 0 to 16384;
+    signal r_wr_count: integer;
+    signal r_rd_count: integer;
 begin
     r_fifo <= uvvm_fifo_init(G_DATA_WIDTH * G_SIZE);
+
+    p_count: process(wr_clk, rst)
+    begin
+        if rst = '1' then
+            r_count <= 0;
+        elsif rising_edge(wr_clk) then
+            r_count <= uvvm_fifo_get_count(r_fifo);
+        end if;
+    end process p_count;
 
     p_full: process(wr_clk, rst)
     begin
@@ -46,7 +59,7 @@ begin
             prog_full <= '1' when uvvm_fifo_get_count(r_fifo) >= G_PROG_FULL
                          else '0';
         end if;
-    end process;
+    end process p_full;
 
     p_empty: process(rd_clk, rst)
     begin
@@ -55,24 +68,41 @@ begin
         elsif rising_edge(rd_clk) then
             empty <= '1' when uvvm_fifo_get_count(r_fifo) = 0 else '0';
         end if;
-    end process;
+    end process p_empty;
 
     p_put: process(wr_clk)
     begin
         if rising_edge(wr_clk) then
-            if wr_en = '1' then
+            if rst = '1' then
+                r_wr_count <= 0;
+            elsif wr_en = '1' then
+                r_wr_count <= r_wr_count + 1;
                 uvvm_fifo_put(r_fifo, din);
             end if;
         end if;
-    end process;
+    end process p_put;
 
     p_get: process(rd_clk)
+        variable v_fwt: boolean := false;
     begin
         if rising_edge(rd_clk) then
-            if rd_en = '1' then
+            if rst = '1' then
+                r_rd_count <= 0;
+            elsif rd_en = '1' then
                 dout <= uvvm_fifo_get(r_fifo, dout'length);
+
+                if uvvm_fifo_get_count(r_fifo) = 0 then
+                    v_fwt := false;
+                end if;
+
+                r_rd_count <= r_rd_count + 1;
+            elsif C_FWT and not v_fwt and uvvm_fifo_get_count(r_fifo) = dout'length then
+                v_fwt := true;
+                dout <= uvvm_fifo_get(r_fifo, dout'length);
+
+                r_rd_count <= r_rd_count + 1;
             end if;
         end if;
-    end process;
+    end process p_get;
 
 end architecture bhv;
